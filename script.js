44921b59f354a15c96b31536f920903225cf859b
Lucas Ruhoff <ruhofflucas@gmail.com>
2026-08-17 16:54:07 -0300

    Adicionando o javascript da calculadora

---
+++ script.js
@@ -0,0 +1,38 @@
+// Dados gerais do IMC
+const dados = [
+  {
+    minimo: 0,
+    maximo: 18.5,
+    classificacao: "Menor que 18,5",
+    info: "Magro",
+    obesidade: "-",
+  },
+  {
+    minimo: 18.5,
+    maximo: 24.9,
+    classificacao: "Entre 18,5 e 24.9",
+    info: "Normal",
+    obesidade: "-",
+  },
+  {
+    minimo: 25,
+    maximo: 29.9,
+    classificacao: "Entre 25 e 29.9",
+    info: "Sobrepeso",
+    obesidade: "I",
+  },
+  {
+    minimo: 30,
+    maximo: 39.9,
+    classificacao: "Entre 30 e 39.9",
+    info: "Obesidade",
+    obesidade: "II",
+  },
+  {
+    minimo: 40,
+    maximo: 99,
+    classificacao: "Acima de 40",
+    info: "Obesidade grave",
+    obesidade: "III",
+  },
+];
